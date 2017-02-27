7ba50ddd0624ef2cfda1bbabf06b2b0edae9e756
Simon Conway <simon.conway@condaal.com>
2017-02-27 12:27:24 +0000

    Initialized git and Rspec, added a blank readme, added lib and spec folders, wrote a test for a class and passed this test'

---
+++ lib/bouncing_balls.rb
@@ -0,0 +1,2 @@
+class BouncingBalls
+end
---
+++ spec/bouncing_balls_spec.rb
@@ -0,0 +1,11 @@
+require 'bouncing_balls'
+
+
+describe BouncingBalls do
+
+it 'should have a class called BouncingBalls' do
+expect(BouncingBalls).to be_a(Class)
+end
+
+
+end
